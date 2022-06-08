8a9329d0298a75c317e93dab90fb3fecf4783cfb
Gaspary23 <pgaspary23@gmail.com>
2022-06-07 21:36:50 -0300

    feat: create rayon runtime file

--- gif-encoder/src/main.rs
+++ gif-encoder/src/main.rs
@@ -2,7 +2,8 @@ use std::env;
 
 mod sequential;
 // mod rust_ssp;
-// mod tokio;
+mod tokio;
+mod rayon;
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -12,12 +13,14 @@ fn main() -> std::io::Result<()> {
     }
 
     let runtime = &args[1];
-    // let threads = args[2].parse::<usize>().unwrap();
+    let threads = args[2].parse::<usize>().unwrap();
     let filename = &args[3];
 
     match runtime.as_str() {
         "sequential" => sequential::encode_gif(&filename).unwrap(),
         // "rust-ssp" => rust_ssp::encode_gif(&filename),
+        "rayon" => rayon::encode_gif(&filename, threads).unwrap(),
+        "tokio" => tokio::encode_gif(&filename, threads).unwrap(),
         _ => panic!("Invalid runtime, use: sequential | rust-ssp")
     }
 
