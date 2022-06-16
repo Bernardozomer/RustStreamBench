00b5d6fce8cd4aaae834f2d8e01e4c451e91414b
Bernardozomer <bernardobarzottoz@gmail.com>
2022-06-16 18:28:22 -0300

    feat: download log input

--- log-processing/get_inputs.sh
+++ log-processing/get_inputs.sh
@@ -2,9 +2,13 @@
 
 LICENSE_KEY=$1
 FILE_NAME="GeoLite2-City"
-URL="https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=${LICENSE_KEY}&suffix=tar.gz"
+LOG_URL="https://raw.githubusercontent.com/GMAP/DSPBench/master/dspbench-threads/data/http-server.log"
+DB_URL="https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=${LICENSE_KEY}&suffix=tar.gz"
 
-if curl --silent -o inputs/${FILE_NAME}.tar.gz $URL; then
+mkdir inputs
+curl --silent -o inputs/http-server.log $LOG_URL
+
+if curl --silent -o inputs/${FILE_NAME}.tar.gz $DB_URL; then
 	tar -xf inputs/${FILE_NAME}.mmdb.tar.gz -C inputs/
 else
 	echo "Failed to download database"
