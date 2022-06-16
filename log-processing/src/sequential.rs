881e81dbcea5122f0b6192436ed862fa5af2fb73
Bernardozomer <bernardobarzottoz@gmail.com>
2022-06-16 18:18:11 -0300

    fix: ignore database errors

--- log-processing/src/sequential.rs
+++ log-processing/src/sequential.rs
@@ -11,7 +11,6 @@ use maxminddb::geoip2::{City, Country};
 /// Parses a logfile in the [Common Log Format](https://en.wikipedia.org/wiki/Common_Log_Format).
 pub fn parse_log(filename: &str, ip_db_path: &str) -> Result<()> {
     let fp = File::open(filename)?;
-    let reader = maxminddb::Reader::open_readfile(ip_db_path)?;
     // Number of occurrences of each city.
     let mut city_freq = HashMap::<u32, usize>::new();
     // Number of occurrences of each country.
@@ -30,36 +29,16 @@ pub fn parse_log(filename: &str, ip_db_path: &str) -> Result<()> {
             bail!("Illegal entry in file: {}", entry);
         }
 
-        // Get the location of this IP address in order to
-        // count the number of occurrences of each city and country.
-        let ip: IpAddr = fields[0].parse()?;
-
-        let city = reader.lookup::<City>(ip)?.city
-            .context(format!("Failed to lookup city for {}", ip))?;
-
-        let city_id = city.geoname_id
-            .context("Failed to obtain city id")?;
-
-        city_freq.insert(
-            city_id,
-            city_freq.get(&city_id).map_or(1, |v| v.add(1))
-        );
-
-        let country = reader.lookup::<Country>(ip)?.country
-            .context(format!("Failed to lookup country for {}", ip))?;
-
-        let country_id = country.geoname_id
-            .context("Failed to obtain city id")?;
-
-        country_freq.insert(
-            country_id,
-            country_freq.get(&country_id).map_or(1, |v| v.add(1))
+        // Ignore any errors found during parsing of the ip address
+        // because it may not be found in the database.
+        let _ = parse_ip(
+            ip_db_path, &fields, &mut city_freq, &mut country_freq
         );
 
         // Count the number of visits per minute.
         let timestamp: DateTime<FixedOffset> = DateTime::parse_from_str(
             format!("{} {}", fields[3], fields[4]).as_str(),
-            "\\[%d/%b/%Y:%H:%M:%S %z\\]"
+            "[%d/%b/%Y:%H:%M:%S %z]"
         )?.duration_round(Duration::minutes(1))?;
 
         visits_per_min.insert(
@@ -75,6 +54,41 @@ pub fn parse_log(filename: &str, ip_db_path: &str) -> Result<()> {
             status_freq.get(status_code).map_or(1, |v| v.add(1))
         );
     }
+
+    Ok(())
+}
+
+fn parse_ip(
+    ip_db_path: &str, fields: &Vec<&str>,
+    city_freq: &mut HashMap<u32, usize>, country_freq: &mut HashMap<u32, usize>
+) -> Result<()> {
+    let reader = maxminddb::Reader::open_readfile(ip_db_path)?;
+
+    // Get the location of this IP address in order to
+    // count the number of occurrences of each city and country.
+    let ip: IpAddr = fields[0].parse()?;
+
+    let city = reader.lookup::<City>(ip)?.city
+        .context(format!("Failed to lookup city for {}", ip))?;
+
+    let city_id = city.geoname_id
+        .context("Failed to obtain city id")?;
+
+    city_freq.insert(
+        city_id,
+        city_freq.get(&city_id).map_or(1, |v| v.add(1))
+    );
+
+    let country = reader.lookup::<Country>(ip)?.country
+        .context(format!("Failed to lookup country for {}", ip))?;
+
+    let country_id = country.geoname_id
+        .context("Failed to obtain city id")?;
+
+    country_freq.insert(
+        country_id,
+        country_freq.get(&country_id).map_or(1, |v| v.add(1))
+    );
 
     Ok(())
 }
